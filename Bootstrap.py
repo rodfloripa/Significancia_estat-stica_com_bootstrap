1c99dd04b7cf0eba8a1eec43fec5dfe9438abfa5
Rodney Nascimento da Silva <rod_engineer@hotmail.com>
2026-02-09 10:08:56 -0300

    Update Bootstrap.py

--- Bootstrap.py
+++ Bootstrap.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def cohen_d(dados1, dados2):
+    # Cálculo do d de Cohen: (M1 - M2) / DP_agrupado
     d = (np.mean(dados1) - np.mean(dados2)) / np.sqrt((np.var(dados1, ddof=1) + np.var(dados2, ddof=1)) / 2)
     return d
 
@@ -14,10 +15,22 @@ def bootstrap_efeito(dados1, dados2, n_boot=10000, alpha=0.05):
         efeitos_boot.append(cohen_d(boot1, boot2))
     
     ic = np.percentile(efeitos_boot, [alpha/2*100, (1-alpha/2)*100])
-    p_valor = np.mean(np.abs(efeitos_boot) >= np.abs(efeito_obs))
     
+    # Classificação do tamanho do efeito (Critérios de Cohen)
+    d_abs = abs(efeito_obs)
+    if d_abs < 0.2:
+        interpretacao = "desprezível"
+    elif d_abs < 0.5:
+        interpretacao = "pequeno"
+    elif d_abs < 0.8:
+        interpretacao = "médio"
+    else:
+        interpretacao = "grande"
+
     return {
         'd': efeito_obs,
         'ic': ic,
-        'p_valor': p_valor
+        'interpretacao': interpretacao
     }
+
+
